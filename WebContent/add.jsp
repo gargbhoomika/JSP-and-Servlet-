510f13390a22b666a8fcf9eade69ede5bec6f70e
Bhoomika Garg <bhoomiika.garg@gmail.com>
2019-02-03 18:03:00 +0530

    JSTL and SQL Added

--- WebContent/add.jsp
+++ WebContent/add.jsp
@@ -1,5 +1,10 @@
 <%@ page language="java" contentType="text/html; charset=ISO-8859-1"
-    pageEncoding="ISO-8859-1"%>
+    pageEncoding="ISO-8859-1" errorPage="error.jsp"%>
+    
+<%@taglib prefix="sql" uri="http://java.sun.com/jsp/jstl/sql" %>
+<%@taglib prefix="c" uri="http://java.sun.com/jsp/jstl/core" %>
+<%@taglib prefix="fn" uri="http://java.sun.com/jsp/jstl/functions" %>
+
 <!DOCTYPE html PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN" "http://www.w3.org/TR/html4/loose.dtd">
 <html>
 <head>
@@ -7,18 +12,32 @@
 <title>Insert title here</title>
 </head>
 <body>
-<%@page import="java.util.Arrays" %>
-<%
+
+<!--
 
 int i = Integer.parseInt(request.getParameter("Num1"));
 int j = Integer.parseInt(request.getParameter("Num2"));
-
 int k = i+j;
-
+int l = i/j;
 out.println("Output is: "+k);
-
-%>
-<%! int c = 0; %>
-
+out.println("DIvision is: "+l); 
+   ${students}-->
+   
+ <!--<c:forEach items="${students}" var="s">
+   ${s.name}<br/>
+   </c:forEach>  -->  
+   
+   <c:set var="str" value="Bhoomika is a java developer"/>
+   
+   Length: "${fn:length(str)}"
+   
+   
+   <sql:setDataSource var="db" driver="com.mysql.jdbc.Driver" url="jdbc:mysql://localhost:3306/test" user="root" password="Bhoomi13@"/> 
+   <sql:query var="rs" dataSource="${db}">Select * from testtable where age=21</sql:query>
+   <c:forEach items="${rs.rows}" var="test">
+   	<c:out value="${test.name}"></c:out><br>
+   	<c:out value="${test.age}"></c:out><br>
+   	<c:out value="${test.sex}"></c:out><br>
+   </c:forEach>
 </body>
 </html>
--- src/co/calculator/AddServlet.java
+++ src/co/calculator/AddServlet.java
@@ -1,7 +1,8 @@
 package co.calculator;
-
+import java.util.List;
 import java.io.IOException;
 import java.io.PrintWriter;
+import java.util.Arrays;
 
 import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
@@ -12,30 +13,35 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 
-//@WebServlet("/add")
+@WebServlet("/add")
 
 public class AddServlet extends HttpServlet {
 
-	public void doGet(HttpServletRequest req, HttpServletResponse res ) throws IOException, ServletException {
-		int i = Integer.parseInt(req.getParameter("Num1"));
-		int j = Integer.parseInt(req.getParameter("Num2"));
+	public void doGet(HttpServletRequest request, HttpServletResponse response ) throws IOException, ServletException {
+		int i = Integer.parseInt(request.getParameter("Num1"));
+		int j = Integer.parseInt(request.getParameter("Num2"));
 		
 		int k = i+j;
-		PrintWriter out = res.getWriter();
+		PrintWriter out = response.getWriter();
 		
 //		HttpSession session = req.getSession();
 //		session.setAttribute("k",k);
 		
-		Cookie cookie = new Cookie("k",k+"");
-		res.addCookie(cookie);
-		
-		out.println("Sum is: "+k);
+//		Cookie cookie = new Cookie("k",k+"");
+//		res.addCookie(cookie);
+//		
+//		out.println("Sum is: "+k);
 		
 //		res.sendRedirect("sq");
-
 		
-//		RequestDispatcher rd = req.getRequestDispatcher("sq");
-//		rd.forward(req, res);
+		
+		List<Student> studs = Arrays.asList(new Student(1,"Bhoomika"),new Student(2,"Bhavy"));
+		
+		Student s = new Student(1,"Bhoomika");
+		
+		request.setAttribute("students", studs);
+		RequestDispatcher rd = request.getRequestDispatcher("add.jsp");
+		rd.forward(request, response);
 
 	}
 
